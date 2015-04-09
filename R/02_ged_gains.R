7d757d27e5323f7b06db4dbc82bc5029fc990f9a
your_username <your_email@example.com>
2015-04-09 08:57:03 -0400

    Added detailed dataset to the returned list

--- R/02_ged_gains.R
+++ R/02_ged_gains.R
@@ -8,24 +8,25 @@
 #' @examples
 #' pos <- laycUtils::load_txt('./my_data_folder/pos.txt')
 #' pos <- laycUtils::format_data(pos)
-#' 
+#'
 #' ged_gains(pos_data = pos)
 
-ged_gains <- function(pos_data, 
+ged_gains <- function(pos_data,
                       eto_programs = c("ss - ged", "pg - employment ged", "dc - wise ged")){
-  
+
   # 1 - keep only relevant records for analysis -----------------------------
   pos_data <- pos_data[pos_data$program_name %in% eto_programs, ]
   pos_data <- pos_data[pos_data$pos_name == "practice ged *total* score", ]
   pos_data$pos_value <- as.numeric(gsub("[^0-9.-]+", "", as.character(pos_data$pos_value)))
   colnames(pos_data)[colnames(pos_data) == 'pos_value'] <- 'score'
-  
+
   # 2 - Identify pre / post scores-------------------------------------------
   pos_data <- id_prepost(pos_data)
-  
+
   # 3 - Compute change in scores---------------------------------------------
   pos_data <- compute_change(pos_data)
-  
+  out <- pos_data
+
   # 4 - Return basic statistics---------------------------------------------
   pos_data <- pos_data[!is.na(pos_data$change_ord), ]
   # Participants showing positive change
@@ -36,9 +37,9 @@ ged_gains <- function(pos_data,
   neg <- length(unique(pos_data$subject_id[pos_data$change_ord == 'negative']))
   # Participants with matching pre post test
   total <- pos + none + neg
-  
+
   # Return dataframe
-  return(list(positive = pos, no_change = none, negative = neg, total = total ))
+  return(list(positive = pos, no_change = none, negative = neg, total = total, full_data = out))
 }
 
 
